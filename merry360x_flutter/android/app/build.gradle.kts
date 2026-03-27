a07017b297a6b5b87a8c4ad0e417243b94ad87ff
vincinci <vincinci@example.com>
2026-03-27 14:54:31 +0200

    fix: update bundle ID to com.merry360x.mobile (matches App Store Connect record #6759515188)

--- merry360x_flutter/android/app/build.gradle.kts
+++ merry360x_flutter/android/app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 }
 
 android {
-    namespace = "com.example.merry360x_flutter"
+    namespace = "com.merry360x.mobile"
     compileSdk = flutter.compileSdkVersion
     ndkVersion = flutter.ndkVersion
 
@@ -21,7 +21,7 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.example.merry360x_flutter"
+        applicationId = "com.merry360x.mobile"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
         minSdk = flutter.minSdkVersion
